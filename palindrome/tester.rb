fa6373df504c35134788964120c27ea61b7017da
mitchashby16 <mitchashby16@gmail.com>
2017-08-14 16:37:24 -0400

    made 2 different palindrome solutions and fixed rspec specs

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -1,13 +1,20 @@
 class Tester
   class T1
     def palindrome?(string)
-      # first implementation
+      return false if string.class != String || string.empty?
+      formatted_string = string.downcase.gsub(/\W/, '')
+      formatted_string == formatted_string.reverse
     end
   end
 
   class T2
     def palindrome?(string)
-      # second implementation
+      begin
+        str = string.downcase.gsub(/\W/, '')
+        return true if (0 ... str.length/2).all? { |i| str[i] == str[-i - 1] } unless string.empty?
+      rescue
+      end
+      return false
     end
   end
 end
